71d69ab253d596945b1e03e0dbe5796faa634e20
Aden de Leeuw <adend@qualtrics.com>
2020-04-04 17:05:03 +1100

    Updates

---
+++ precache-manifest.caeff7453b4623108fc373f1f3552018.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1d30d538d288dc39d09b88f549c0a5f",
+    "url": "/monstersrolodex/index.html"
+  },
+  {
+    "revision": "d0f36f3e281b641f89d8",
+    "url": "/monstersrolodex/static/css/main.0fc78709.chunk.css"
+  },
+  {
+    "revision": "e426afe3323271f770fe",
+    "url": "/monstersrolodex/static/js/2.3cce4cea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monstersrolodex/static/js/2.3cce4cea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0f36f3e281b641f89d8",
+    "url": "/monstersrolodex/static/js/main.5dad9da7.chunk.js"
+  },
+  {
+    "revision": "e2b87f5cbc5fe6b62520",
+    "url": "/monstersrolodex/static/js/runtime-main.4fc72c3f.js"
+  }
+]);
